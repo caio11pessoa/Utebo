c986ad7fc4974f2f503fa437652dfaf3acbdbe1f
Caio Pessoa <caio.almeida.pessoa07@aluno.ifce.edu.br>
2021-02-09 18:37:12 -0300

    Modificando o tamanho da tela e melhorando nomes de variáveis

--- jogo.py
+++ jogo.py
@@ -1,10 +1,10 @@
 import sys, pygame
 pygame.init()
-size = width, height = 1000, 700
-black = 124, 252, 000
+size = width, height = 1200, 680
+color = 124, 252, 000
 screen = pygame.display.set_mode(size)
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    screen.fill(black)
+    screen.fill(color)
     pygame.display.flip()
