6eb511b86d748b5a6d6fc865d4e57fb8f25fc6bd
Caio Pessoa <caio.almeida.pessoa07@aluno.ifce.edu.br>
2021-02-09 21:59:33 -0300

    adicionando layout de fundo de jogo

--- jogo.py
+++ jogo.py
@@ -1,10 +1,29 @@
 import sys, pygame
+
+def retornaLayout (tela, corLinhas, largura, altura):
+    pygame.draw.line(tela, corLinhas, [largura/2, 0], [largura/2, altura], 6)#linha do meio
+    pygame.draw.line(tela, corLinhas, [0, 2], [largura, 2], 6) #linha de cima
+    pygame.draw.line(tela, corLinhas, [2, 0], [2, altura], 6) #linha da esquerda
+    pygame.draw.line(tela, corLinhas, [0, altura-4], [largura, altura-4], 6) #linha de baixo
+    pygame.draw.line(tela, corLinhas, [largura-4, 0], [largura-4, altura], 6) #linha da direita
+    pygame.draw.ellipse(tela, corLinhas, [largura/2 - altura/6, altura/2-altura/6, altura/3, altura/3] , 6) #bolota do meio
+    pygame.draw.rect(tela, corLinhas, [2, altura/4, (largura/4)-50, altura/2], 6) #trave da esquerda
+    pygame.draw.rect(tela, corLinhas, [(largura-4)-(largura/4 -50), altura/4, (largura/4)-50, altura/2], 6) #trave da direita
+
 pygame.init()
-size = width, height = 1200, 680
-color = 124, 252, 000
-screen = pygame.display.set_mode(size)
+tamanho = largura, altura = 1350, 680
+color = 110, 222, 000
+corLinhas = 242, 242,242
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption('UTEBO')
+
+tela.fill(color)
+
+retornaLayout(tela,corLinhas, tamanho[0], tamanho[1])
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    screen.fill(color)
+
     pygame.display.flip()
+    
+    
